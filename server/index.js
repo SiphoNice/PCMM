c726a0555acafc51c84de7e11626bb80e428f14d
Derrick <houseinthehills2020@gmail.com>
2023-09-21 15:39:29 +0200

    feat: created insert and get functionality

--- server/index.js
+++ server/index.js
@@ -10,18 +10,28 @@ const client = new Client({
   database: "pcmm_assignment",
 });
 client.connect();
-client.query("select * from contacts", (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err.message);
-  }
-  client.end;
-});
-
 app.use(cors());
 app.use(express.json());
-
+app.post("/create_safety_incident", async (req, res) => {
+  try {
+    const { mine_id, latitude, longitude, description, severity } = req.body;
+    const add_incident = await client.query(
+      "INSERT INTO incidents(mine_id,latitude,longitude,description,severity) VALUES($1,$2,$3,$4,$5) RETURNING * ",
+      [mine_id, latitude, longitude, description, severity]
+    );
+    res.json(add_incident.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+app.get("safety_incident", async (req, res) => {
+  try {
+    const safety_incident = await client.query("SELECT * FROM incidents");
+    res.json(safety_incident.rows);
+  } catch (err) {
+    console.log(err.log);
+  }
+});
 app.get("/contacts", (req, res) => {
   res.json({ contacts: ["contact 1", "contact 2"] });
 });
